8f458042fc20a4462e4559080f61648b3d1965f0
justin <dancinglinkxalgorithm@gmail.com>
2019-05-27 03:34:43 +0800

    Feat(download.py): save all musics info into list

--- download.py
+++ download.py
@@ -33,16 +33,31 @@ else:
     }
     course = r.get(url + "/course.php", params=payloads)
     course.encoding = "utf-8"
-    # print (course.text)
+    """ Debug
+    print (course.text)
+    """
 
 soup = BeautifulSoup(course.text, "lxml")
 block = soup.find("div", class_="block")
+musics = []
 
 for div in block.find_all("div"):
-    print ("downloading ... " + div.a["title"])
+    title = div.a["title"]
+    print ("downloading ... " + title)
     d_url = url + div.a["href"]
-    d_file = "./assets/" + div.a["title"]
-
+    number = title[0:3]
+    filename = title[3:].split(".")[-2]
+    breakpoint = filename.find("(") != -1 or filename.find("（")
+    race = filename[breakpoint + 1:filename.find("族") + 1]
+    d_file = "./assets/" + filename
+    music = {
+        "index": number,
+        "race": race,
+        "filename": filename,
+        "url": d_file
+    }
+    musics.append(music)
+    
     with requests.get(d_url, stream=True, cookies=r.cookies) as res:
         res.raise_for_status()
         with open(d_file, "wb") as f:
@@ -53,6 +68,6 @@ for div in block.find_all("div"):
     print ("done")
 
 print ("Finished, all mp3 files are stored in assets folder.")
-
+print (musics)
 
 
